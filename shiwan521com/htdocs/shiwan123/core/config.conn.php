0e6b85cc78d2a98bb1d1393d3ba894aabbc05373
“your_udengying <your_registere542593802@qq.com>
2019-04-13 22:54:10 +0800

    shiwan321:appcms

---
+++ shiwan521com/htdocs/shiwan123/core/config.conn.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AppCMS: (C)2013  
+ * AppCMS is a free open source mobile phone APP application download website content management system.
+ * Custom development, production BUG report template, please contact loyjers@qq.com
+ * Author: crane
+ * Editor: crane
+ */
+
+/**
+ * 定义数据库配置参数
+ */
+// 数据库host名称
+define('DB_HOST', 'xdm576923346.my3w.com');
+// 数据库用户名
+define('DB_USERNAME', 'xdm576923346');
+//数据库密码
+define('DB_PASS', 'Qwer@1234');
+//数据库
+define('DB_DBNAME', 'xdm576923346_db');
+//数据库编码
+define('DB_CHARSET', 'utf8');
+//表名前缀
+define('TB_PREFIX', 'appcms_');
+

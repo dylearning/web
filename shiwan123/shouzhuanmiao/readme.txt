72bec1ee930b6e17ce9916173b9736c5bd11a1db
dengying <542593802@qq.com>
2019-10-06 12:18:12 +0800

    爱盈利更新二维码

--- shiwan123/shouzhuanmiao/readme.txt
+++
@@ -1,6 +0,0 @@
-admin admin
-
-  'ZC_MYSQL_SERVER' => 'xdm576923346.my3w.com',
-  'ZC_MYSQL_USERNAME' => 'xdm576923346',
-  'ZC_MYSQL_PASSWORD' => 'Qwer@1234',
-  'ZC_MYSQL_NAME' => 'xdm576923346_db',
--- shiwan123/shouzhuanmiao/templates/style/inc_foot.php
+++
@@ -1,4 +0,0 @@
-	<div class="box wrap-footer">
-
-<span style="font-size:9px;">Powered by &nbsp;&nbsp;&nbsp;<a href="http://www.yixiangzhuan.com">手机赚钱软件</a>&nbsp;&nbsp;<?php echo('Processed in '.number_format(helper::getmicrotime()-$time_start, 2, '.', '').' ms ');?>&nbsp;&nbsp;<?php if(defined('COUNT_CODE')) echo(COUNT_CODE); ?></span>
-</div>
